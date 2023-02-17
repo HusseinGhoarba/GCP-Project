9ba4d04589437d6847ec65a71b1e416fbc5ca9d8
Hussein Ghoraba <husseinghoraba.gh@gmail.com>
2023-02-17 22:42:07 +0200

    upload the terraform module of the gke-cluster

--- terraform-files/gke-cluster/main.tf
+++ terraform-files/gke-cluster/main.tf
@@ -1,46 +1,50 @@
-#creating private cluster ans associate it to restricted subnet
-resource "google_container_cluster" "my-private-cluster" {
-  name     = "my-private-cluster"
-  location = "us-east1-b"
-  # creating the least possible node pool  
-  remove_default_node_pool = true
-  initial_node_count       = 1
-  network                  = google_compute_network.python-app-vpc.id
-  subnetwork               = google_compute_subnetwork.restricted_subnet.id
-
-
+#-------------------------------Creating -private- GKE -cluster- on restricted subnet
+resource "google_container_cluster" "default-cluster" {
+  name     = var.cluster-name
+  location = var.cluster-zone
+  #-----------------------------------------------  
+  remove_default_node_pool = var.remove-default-node-pool
+  initial_node_count       = var.initial-node-count
+  network                  = var.vpc-id-for-cluster
+  subnetwork               = var.subnet-id-for-cluster
+  #-----------------------------------------------
   master_authorized_networks_config {
     cidr_blocks {
-      cidr_block   = "10.0.0.0/24"
-      display_name = "managment-cidr-range"
+      cidr_block   = var.master-cidr-ip-block
+      display_name = var.master-name-config
     }
   }
-
+  #-----------------------------------------------
   ip_allocation_policy {
     cluster_ipv4_cidr_block  = "10.16.0.0/16"
     services_ipv4_cidr_block = "10.12.0.0/16"
   }
-
-
+  #-----------------------------------------------
   private_cluster_config {
     enable_private_nodes    = true
     enable_private_endpoint = true
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
-
+  #-----------------------------------------------
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+  }
 }
 
+#---------------Creation of NODE Pool and attach it to Cluster
 resource "google_container_node_pool" "nodepool" {
-  name       = "nodepool"
-  location   = "us-east1-a"
-  cluster    = google_container_cluster.my-private-cluster.id
-  node_count = 1
-
+  name       = var.name-of-node
+  location   = var.location-of-node
+  cluster    = google_container_cluster.default-cluster.id
+  node_count = var.count-of-node
+  #-----------------------------------------------
   node_config {
     preemptible  = true
-    machine_type = "e2-micro"
-    # custom service account
-    service_account = google_service_account.project-service-account.email
+    machine_type = var.machine-type-of-node
+    #-------------------------------service account
+    service_account = var.email-of-sa-for-node
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
---
+++ terraform-files/gke-cluster/variables.tf
@@ -0,0 +1,32 @@
+#---------------------------Creation of Cluster
+variable "cluster-name" { 
+}
+variable "cluster-zone" {  
+}
+#-----------------------------------------------  
+variable "remove-default-node-pool" { 
+}
+variable "initial-node-count" {
+}
+variable "vpc-id-for-cluster" {
+}
+variable "subnet-id-for-cluster" {  
+}
+#-----------------------------------------------  
+variable "master-cidr-ip-block" {
+}
+variable "master-name-config" {
+}
+#-----------------------------------------------
+#---------------------------Creation of Cluster
+variable "name-of-node" {
+}
+variable "location-of-node" {
+}
+variable "count-of-node" {
+}
+variable "machine-type-of-node" {
+}
+variable "email-of-sa-for-node" {
+}
+#-----------------------------------------------
